ff5830436aef698c9549e4b49d17f99c2fcf7adc
George Wyscaver <gwyscaver3@gmail.com>
2019-10-29 19:40:11 -0700

    Updates

---
+++ precache-manifest.cf7bea579a730256aa9bb682f0c66c85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c76decd7034bbc9fbbebcfc9f911f2f3",
+    "url": "/nyacht_project3/index.html"
+  },
+  {
+    "revision": "88c1fa5ee870e9004e9a",
+    "url": "/nyacht_project3/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "53c7c0225090d4b6086c",
+    "url": "/nyacht_project3/static/js/2.09320111.chunk.js"
+  },
+  {
+    "revision": "88c1fa5ee870e9004e9a",
+    "url": "/nyacht_project3/static/js/main.92e2af2d.chunk.js"
+  },
+  {
+    "revision": "dba35f9173f1db5ac7e4",
+    "url": "/nyacht_project3/static/js/runtime-main.187a5348.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/nyacht_project3/static/media/logo.25bf045c.svg"
+  }
+]);
